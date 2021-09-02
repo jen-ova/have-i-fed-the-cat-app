7bf3c7cc4dae54a197ba7a2e4e8ac3d504b9bd0f
Jen Stewart <jenstewart2404@gmail.com>
2021-09-02 15:19:51 +0100

    Added optional feed patch with timestamp, delete cat & error handling

--- src/app.js
+++ src/app.js
@@ -6,21 +6,53 @@ const app = express();
 app.use(express.json());
 
 app.post("/cats", (req, res) => {
-	Cat.create(req.body).then((cat) => res.status(201).json(cat));
+	Cat.create(req.body)
+		.then((cat) => res.status(201).json(cat))
+		.catch((err) => {
+			res.status(500).jason(err);
+		});
 });
 
 app.get("/cats", (req, res) => {
-	Cat.findAll({ where: req.query }).then((cat) => res.status(200).json(cat));
+	Cat.findAll({ where: req.query })
+		.then((cat) => res.status(200).json(cat))
+		.catch((err) => {
+			res.status(500).jason(err);
+		});
 });
 
 app.get("/cats/:catId", (req, res) => {
-	Cat.findByPk(req.params.catId).then((cat) => res.status(200).json(cat));
+	Cat.findByPk(req.params.catId)
+		.then((cat) => res.status(200).json(cat))
+		.catch((err) => {
+			res.status(500).jason(err);
+		});
 });
 
 app.patch("/cats/:catId", (req, res) => {
-	Cat.update(req.body, { where: { id: req.params.catId } }).then((cat) =>
-		res.status(200).json(cat)
-	);
+	Cat.update(req.body, { where: { id: req.params.catId } })
+		.then((cat) => res.status(200).json(cat))
+		.catch((err) => {
+			res.status(500).jason(err);
+		});
+});
+
+app.patch("/feed/:catId", (req, res) => {
+	Cat.update({ lastFed: new Date() }, { where: { id: req.params.catId } })
+		.then((cat) => res.status(200).json(cat))
+		.catch((err) => {
+			res.status(500).jason(err);
+		});
+});
+
+app.delete("/cats/:catId", (req, res) => {
+	Cat.destroy({ where: { id: req.params.catId } })
+		.then((catsDeleted) => {
+			res.status(200).send({ catsDeleted });
+		})
+		.catch((err) => {
+			res.status(500).jason(err);
+		});
 });
 
 module.exports = app;
