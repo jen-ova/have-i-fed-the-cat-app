6e4066c5b0e3bdf18a62b63b04996351de6a453f
Jen Stewart <jenstewart2404@gmail.com>
2021-09-06 14:39:09 +0100

    Fixed app.js errors

--- src/app.js
+++ src/app.js
@@ -1,57 +1,57 @@
-const express = require("express");
-const { Cat } = require("./models");
+const express = require('express');
+const { Cat } = require('./models');
 
 const app = express();
 
 app.use(express.json());
 
-app.post("/cats", (req, res) => {
+app.post('/cats', (req, res) => {
 	Cat.create(req.body)
 		.then((cat) => res.status(201).json(cat))
 		.catch((err) => {
-			res.status(500).jason(err);
+			res.status(500).json(err);
 		});
 });
 
-app.get("/cats", (req, res) => {
+app.get('/cats', (req, res) => {
 	Cat.findAll({ where: req.query })
 		.then((cat) => res.status(200).json(cat))
 		.catch((err) => {
-			res.status(500).jason(err);
+			res.status(500).json(err);
 		});
 });
 
-app.get("/cats/:catId", (req, res) => {
+app.get('/cats/:catId', (req, res) => {
 	Cat.findByPk(req.params.catId)
 		.then((cat) => res.status(200).json(cat))
 		.catch((err) => {
-			res.status(500).jason(err);
+			res.status(500).json(err);
 		});
 });
 
-app.patch("/cats/:catId", (req, res) => {
+app.patch('/cats/:catId', (req, res) => {
 	Cat.update(req.body, { where: { id: req.params.catId } })
 		.then((cat) => res.status(200).json(cat))
 		.catch((err) => {
-			res.status(500).jason(err);
+			res.status(500).json(err);
 		});
 });
 
-app.patch("/feed/:catId", (req, res) => {
+app.patch('/feed/:catId', (req, res) => {
 	Cat.update({ lastFed: new Date() }, { where: { id: req.params.catId } })
 		.then((cat) => res.status(200).json(cat))
 		.catch((err) => {
-			res.status(500).jason(err);
+			res.status(500).json(err);
 		});
 });
 
-app.delete("/cats/:catId", (req, res) => {
+app.delete('/cats/:catId', (req, res) => {
 	Cat.destroy({ where: { id: req.params.catId } })
 		.then((catsDeleted) => {
 			res.status(200).send({ catsDeleted });
 		})
 		.catch((err) => {
-			res.status(500).jason(err);
+			res.status(500).json(err);
 		});
 });
 
